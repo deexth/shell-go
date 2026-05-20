3658b0eca8c09b18f51e59b844553e6dc32c9da1
deexth <hkabare@gmail.com>
2026-05-20 11:10:25 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	fmt.Fprintf(os.Stderr, "%s: command not found\n", scanner.Text())
+	for {
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error reading input: ", err)
+			os.Exit(1)
+		}
+
+		fmt.Fprintf(os.Stderr, "%s: command not found\n", command[:len(command)-1])
+	}
 }
